9ca4eaa2d86087a47e112cfe5844c3131d519c3e
pradeep.b <pradeep.b@lucidsolutions.in>
2023-07-07 19:32:11 +0530

    changes

--- client-vue/src/main.js
+++ client-vue/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
+import { createApp, Vue } from 'vue'
 import './index.css'
 import App from './App.vue'
+import Argon from '@/plugins/argon-kit'
+Vue.use(Argon)
 // import axios
 /* import axios from 'axios'
 // set a prototype for http
